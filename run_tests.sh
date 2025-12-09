1ce6c5475a478c4ebafe473c756f5cd0b92670a5
droidkid <yuvaraj.chesetti@gmail.com>
2025-12-08 23:53:42 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,6 @@ python3 ./bench/bench_filters.py  -F paper/normal-microbenchmark with num_querie
 python3 ./bench/bench_filters.py  -F paper/lognormal-microbenchmark with num_queries=500000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=lognormal break_even=2 collect_db_stats=True microbench=true quotient_bits=27
 python3 ./bench/bench_filters.py  -F paper/zipf-microbenchmark with num_queries=500000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=zipfian break_even=2 collect_db_stats=True quotient_bits=27 microbench=true capture_extra_stats=True zipf_constant=0.99 storage_cache_size_mb=64
 
-exit 0
 
 python3 ./bench/bench_filters.py  -F paper/adversarial_advFreq-5_cache-64 with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=adversarial break_even=2 collect_db_stats=True quotient_bits=27 storage_cache_size_mb=64 adv_freq=5
 python3 ./bench/bench_filters.py  -F paper/adversarial_advFreq-10_cache-64 with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=adversarial break_even=2 collect_db_stats=True quotient_bits=27 storage_cache_size_mb=64 adv_freq=10
@@ -15,8 +14,6 @@ python3 ./bench/bench_filters.py  -F paper/uniform-cdf with num_queries=10000000
 python3 ./bench/bench_filters.py  -F paper/normal-cdf with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=normal break_even=2 collect_db_stats=True microbench=true quotient_bits=27 workload_only=True
 python3 ./bench/bench_filters.py  -F paper/lognormal-cdf with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=lognormal break_even=2 collect_db_stats=True microbench=true quotient_bits=27 workload_only=True
 python3 ./bench/bench_filters.py  -F paper/zipf-cdf with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=zipfian break_even=2 collect_db_stats=True quotient_bits=27 microbench=true capture_extra_stats=True zipf_constant=0.99 storage_cache_size_mb=1 workload_only=True
-
-exit 0
 
 python3 ./bench/bench_filters.py  -F paper/zipf with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=zipfian break_even=2 collect_db_stats=True quotient_bits=27 capture_extra_stats=True zipf_constant=0.99 storage_cache_size_mb=64
 python3 ./bench/bench_filters.py  -F paper/normal with num_queries=100000000 num_rounds=100 storage_engine=wiredTiger reverse_map_engine=wiredTiger query_workload=normal break_even=2 collect_db_stats=True quotient_bits=27 capture_extra_stats=True
